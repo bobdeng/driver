5746097a8eb9e6b81bf3b2406ab4f08f4acd64dd
bobdeng <bobdeng@163.com>
2018-06-29 12:14:12 +0800

    remove test

--- server/src/main/java/cn/bobdeng/line/driver/server/controller/VersionControler.java
+++ server/src/main/java/cn/bobdeng/line/driver/server/controller/VersionControler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionControler {
 
-    public static final String KEY_123 = "key2";
     @Value("${build.version}")
     String version;
     @Autowired
@@ -21,7 +20,6 @@ public class VersionControler {
 
     @GetMapping("/version")
     public String version() {
-        System.out.println(redissonClient.getAtomicLong(KEY_123).compareAndSet(0,100));
         return version;
     }
 }

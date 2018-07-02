956a7c742e64049a2e27c447982370e4d2e14fa2
bobdeng <bobdeng@163.com>
2018-07-02 22:12:36 +0800

    add final content result

--- domain/src/main/java/cn/bobdeng/line/driver/domain/message/Message.java
+++ domain/src/main/java/cn/bobdeng/line/driver/domain/message/Message.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Iterator;
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,5 +18,15 @@ public class Message {
     private int orgId;
     private String type;
     private String content;
+    private Map<String,String> values;
     private long createTime;
+    public String getFinalContent(){
+        String result=content;
+        Iterator<Map.Entry<String, String>> iterator = values.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<String, String> entry = iterator.next();
+            result = result .replaceAll("\\{"+entry.getKey()+"\\}",entry.getValue());
+        }
+        return result;
+    }
 }
--- server/src/main/java/cn/bobdeng/line/driver/server/message/MessageRepositoryImpl.java
+++ server/src/main/java/cn/bobdeng/line/driver/server/message/MessageRepositoryImpl.java
@@ -5,10 +5,12 @@ import cn.bobdeng.line.driver.domain.message.Message;
 import cn.bobdeng.line.driver.domain.message.MessageRepository;
 import cn.bobdeng.line.driver.server.dao.MessageDAO;
 import cn.bobdeng.line.driver.server.dao.MessagePoolDAO;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -27,12 +29,13 @@ public class MessageRepositoryImpl implements MessageRepository {
     private Message fromDO(MessageDO messageDO) {
         Message message = new Message();
         BeanUtils.copyProperties(messageDO, message);
+        message.setValues((Map<String, String>) JSONObject.parse(messageDO.getValues()));
         return message;
     }
 
     @Override
     public void updateConfirmTime(int id, int userId, long millis) {
-        messageDAO.updateConfirmTime(id,userId,millis);
+        messageDAO.updateConfirmTime(id, userId, millis);
     }
 
     @Override

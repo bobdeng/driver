48682b887d65cdf2e6d9f3d204fa41bc56831265
bobdeng <bobdeng@163.com>
2018-07-04 08:51:21 +0800

    remove dao

--- server/src/main/java/cn/bobdeng/line/driver/server/dao/CounterDAO.java
+++
@@ -1,7 +0,0 @@
-package cn.bobdeng.line.driver.server.dao;
-
-import cn.bobdeng.line.db.CounterDO;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CounterDAO extends CrudRepository<CounterDO,Integer> {
-}

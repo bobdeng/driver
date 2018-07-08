2cc35265a42271b044b2a60f8297d5b495b5c38f
bobdeng <bobdeng@163.com>
2018-07-09 06:48:17 +0800

    add number list test

---
+++ server/src/main/java/cn/bobdeng/line/driver/server/numbers/NumbersController.java
@@ -0,0 +1,20 @@
+package cn.bobdeng.line.driver.server.numbers;
+
+import com.tucodec.rest.CommonResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/numbers")
+public class NumbersController {
+    @GetMapping("/list")
+    public CommonResponse<List<PhoneNumberVO>> list(){
+        return CommonResponse.getSuccess(Arrays.asList(
+                PhoneNumberVO.builder().name("秩序宝通知（勿接）").number("4008001456").build()
+        ));
+    }
+}
---
+++ server/src/main/java/cn/bobdeng/line/driver/server/numbers/PhoneNumberVO.java
@@ -0,0 +1,15 @@
+package cn.bobdeng.line.driver.server.numbers;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PhoneNumberVO {
+    private String name;
+    private String number;
+}
